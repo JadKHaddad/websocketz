e8d9cccaa8a58039b6e720fd4d9cff791f74cc4b
jha <jad.haddad@gselectronic.com>
2025-06-20 10:45:42 +0200

    feat(example): updated simple example
    Signed-off-by: jha <jad.haddad@gselectronic.com>

--- examples/simple.rs
+++ examples/simple.rs
@@ -9,14 +9,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_env_filter("framez=trace")
         .init();
 
-    let stream = TcpStream::connect("127.0.0.1:8080").await?;
+    let stream = TcpStream::connect("127.0.0.1:9002").await?;
 
     let read_buf = &mut [0u8; 8192 * 2];
     let write_buf = &mut [0u8; 8192 * 2];
     let fragments_buf = &mut [0u8; 8192 * 2];
     let rng = StdRng::from_os_rng();
 
-    let websocketz = Websockets::connect::<16>(
+    let mut websocketz = Websockets::connect::<16>(
         "/",
         &[],
         FromTokio::new(stream),
@@ -32,20 +32,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         websocketz.framable()
     );
 
-    let (mut websocketz_read, mut websocketz_write) = websocketz.split_with(|stream| {
-        let (read, write) = tokio::io::split(stream.into_inner());
-        (FromTokio::new(read), FromTokio::new(write))
-    });
+    websocketz.send(Message::Text("Hello, WebSocket!")).await?;
 
-    websocketz_write
-        .send(Message::Text("Hello, WebSocket!"))
-        .await?;
-
-    websocketz_write
+    websocketz
         .send_fragmented(Message::Text("Hello, Fragmented WebSocket!"), 4)
         .await?;
 
-    while let Some(message) = next!(websocketz_read).transpose()? {
+    while let Some(message) = next!(websocketz).transpose()? {
         println!("Received message: {message:?}");
     }
 
